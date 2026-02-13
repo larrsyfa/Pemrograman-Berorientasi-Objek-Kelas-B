1e8f5226fe1808e0a3629a52c320bc7bfefbd30c
larrsyfa <cipaws0u0@gmail.com>
2026-02-13 08:18:54 +0700

    pertemuan ke 2

---
+++ pertemuan 2/p023WorkingVariable.java
@@ -0,0 +1,17 @@
+public class p023WorkingVariable {
+    public static void main(String[] args){
+        int umur = 20;
+        long populasi = 8000000000L;
+        float tinggi = 170.5f;
+        double ipk = 3.75;
+        char nilaiHuruf = 'A';
+        boolean lulus = true;
+
+        System.out.println("Umur          : " + umur);
+        System.out.println("Populasi      : " + populasi);
+        System.out.println("Tinggi        : " + tinggi);
+        System.out.println("IPK           : " + ipk);
+        System.out.println("Nilai Huruf   : " + nilaiHuruf);
+        System.out.println("Lulus         : " + lulus);
+    }
+}
