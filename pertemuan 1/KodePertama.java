cdb52517ba8edcfdd2b4f6790284b53a18d0784d
larrsyfa <cipaws0u0@gmail.com>
2026-02-13 08:07:05 +0700

    pertemuan ke 1

---
+++ pertemuan 1/KodePertama.java
@@ -0,0 +1,7 @@
+public class KodePertama {
+    public static void main(String[] args){
+        System.out.println("Halo, Pemrograman Berorientasi Objek!");
+        System.out.println("Ini adalaha program Java pertama saya.");
+    }
+}
+
---
+++ pertemuan 1/KodePertama_Latihan1.java
@@ -0,0 +1,13 @@
+/*
+Nama    : Larra Syifa As Shaum
+NIM     : D1041241070
+Tanggal : 5 Februari 2026
+*/
+public class KodePertama_Latihan1 {
+    public static void main (String[] args){
+        System.out.println("Nama : Larra Syifa As Shaum");
+        System.out.println("NIM : D1041241070");
+        System.out.println("Semester : 4");
+        System.out.println("Prodi : Informatika");
+    }
+}
