983f868ba107ca7a76e9f035edab79d2d50a656b
larrsyfa <cipaws0u0@gmail.com>
2026-02-20 11:21:37 +0700

    Latihan Pertemuan 1

--- pertemuan 1 pbo/KodePertama.java
+++ pertemuan 1 pbo/KodePertama.java
@@ -1,3 +1,9 @@
+/*
+Nama    : Larra Syifa As Shaum
+NIM     : D1041241070
+Tanggal : 5 Februari 2026
+*/
+
 public class KodePertama {
     public static void main(String[] args){
         System.out.println("Halo, Pemrograman Berorientasi Objek!");
--- pertemuan 1 pbo/KodePertama_Latihan1.java
+++ pertemuan 1 pbo/KodePertama_Latihan1.java
@@ -3,6 +3,7 @@ Nama    : Larra Syifa As Shaum
 NIM     : D1041241070
 Tanggal : 5 Februari 2026
 */
+
 public class KodePertama_Latihan1 {
     public static void main (String[] args){
         System.out.println("Nama : Larra Syifa As Shaum");
